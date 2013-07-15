9085231d931a2742b5cd2e582bb33c00acbee1a7
Mahendra <gmk834@gmail.com>
2013-07-16 00:37:23 +0530

    I quit

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var fs=require('fs');
 app.get('/', function(request, response) {
   
-  var content = fs.readFileSync('index.html', 'utf8');
+  var content = fs.readFileSync('index.html','utf8');
 });
 
 var port = process.env.PORT || 5000;
