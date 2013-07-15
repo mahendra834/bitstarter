d1c3d8af1824c5afc4f441f152e4f58f28659c49
Mahendra <gmk834@gmail.com>
2013-07-15 23:43:56 +0530

    Index.html added and web.js code changed

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs=require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fs.readFile('index.hml'));
 });
 
 var port = process.env.PORT || 5000;
