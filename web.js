ccfd2abb36a9e8bfec402249cc1292dc686cbb1b
Mahendra <gmk834@gmail.com>
2013-07-16 01:16:07 +0530

    copied

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 
-var data = fs.readFileSync("index.html", "utf-8");
+
 
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(fs.readFileSync("index.html", "utf-8"));
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
